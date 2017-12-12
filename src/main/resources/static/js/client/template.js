111f34014245071ce35989a58dd035e48d27603e
nicky.zhang <nickyzhang@augmentum.com.cn>
2017-12-12 18:08:10 +0800

    update

--- src/main/resources/static/js/client/template.js
+++ src/main/resources/static/js/client/template.js
@@ -36,39 +36,3 @@ function redraw(){
     var img = drawMessage(m_background_image, m_company_name, m_category_name, new Date());
     $("#show_template").attr('src', img);
 }
-
-function drawMessage(background_image,company_name, category_name, deadline, contact, phone_number, explanation, voucher_number){
-    var year = "xxxx";
-    var month = "xx";
-    var day = "xx";
-
-    if(deadline != undefined){
-        year = deadline.getFullYear();
-        month = deadline.getMonth()+1;
-        day =  deadline.getDate();
-    }
-    company_name == undefined ? company_name="大闸蟹公司":company_name;
-    category_name == undefined ? category_name="公蟹4只":category_name;
-    (contact == undefined)? contact="周XX":contact;
-    (phone_number == undefined) ? phone_number="138XXX":phone_number;
-    explanation == undefined?explanation="XX":explanation;
-    voucher_number == undefined?voucher_number="XXXXX":voucher_number;
-
-    var canvas_default = document.createElement("canvas");
-    var ctx = canvas_default.getContext("2d");
-
-    var image_height = background_image.height;
-    var image_width = background_image.width;
-    canvas_default.height = image_height+20;
-    canvas_default.width= image_width+20;
-
-    ctx.drawImage(background_image,10,10, image_width, image_height);
-    ctx.font="39px 微软雅黑 ";
-    ctx.fillStyle="#fff";
-    ctx.fillText(company_name, 0.31*image_width, 0.19*image_height);
-    ctx.fillText(category_name, 0.31*image_width, 0.28*image_height);
-    ctx.fillText(contact, 0.31*image_width, 0.58*image_height);
-    return canvas_default.toDataURL("image/png");
-};
-
-
--- src/main/resources/static/js/client/voucher.js
+++ src/main/resources/static/js/client/voucher.js
@@ -1,3 +1,13 @@
+$(function(){
+    $(".qrcode_td > div").each(function(index, val){
+        var id = val.id;
+        var qrcode = new QRCode(val, {
+            width : 100,
+            height : 100
+        });
+        qrcode.makeCode(id);
+    });
+});
 function toggleCheck(){
     var checked = $("#control_check")[0].checked;
     var voucherChecks = document.getElementsByName("voucher_check");
@@ -8,6 +18,23 @@ function toggleCheck(){
 
 function generatePic(){
     var voucherChecks = document.getElementsByName("voucher_check");
+    var qrcode = new QRCode(document.getElementById("qrcode"), {
+        width : 100,
+        height : 100
+    });
+    qrcode.makeCode("this is text");
 
 
+    var imgData = $("#qrcode img").attr("src");
+
+    var zip = new JSZip();
+    var img = zip.folder("images");
+    img.file("code.png", imgData, {base64: true});
+    zip.generateAsync({type:"blob"})
+    .then(function(content) {
+        // see FileSaver.js
+        saveAs(content, "example.zip");
+    });
 }
+
+
